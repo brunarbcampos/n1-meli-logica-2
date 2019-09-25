64b5c8b15c08f92df0a4f806662511ca0a9b511c
brunarbcampos <brunarbcampos@gmail.com>
2019-09-24 22:04:05 -0300

    exercício 24/09

--- 2_op_ar/exercicio.js
+++ 2_op_ar/exercicio.js
@@ -1,1 +1,19 @@
 //resposta aqui
+
+var r = require("readline-sync");
+
+var produtos = [
+    {produto: "produto1", preco: 50},
+    {produto: "produto2", preco: 100},
+    {produto: "produto3", preco: 150},
+];
+
+var posicao = parseInt(r.question("Escolha um produto entre 0 e 2."));
+var porcentagem = parseInt(r.question("Digite uma porcentagem:"));
+
+
+var produtoSelecionado = produto[posicao];
+
+var desconto = produtoSelecionado.preco * (porcentagem /100);
+
+console.log("Produto" + produtoSelecionado.nome + "com desconto é R$" + desconto.toFixed(2));
--- 3_estrutura/exercicio.js
+++ 3_estrutura/exercicio.js
@@ -1,1 +1,30 @@
-//resposta aqui
+var r = require("readline-sync");
+
+var produtos =[
+    {produto: "lasanha", tempoDeValidade: 20, Congelado: true},
+    {produto: "sorvete", tempoDeValidade: 50, Congelado: true},
+    {produto: "biscoito", tempoDeValidade:80, Congelado: false}
+];
+
+var posicao = parseInt(r.question("Escolha um produto: 0 - lasanha, 1 - sorvete, 2- biscoito:"));
+var validade = parseInt(r.question("Digite quantos dias faltam pra vencer:"));
+
+var produtoSelecionado =  produtos[posicao];
+
+var porcentagem = (validade * 100) / produtoSelecionado.tempoDeValidade;
+
+if (porcentagem == 2){
+    console.log("Descartar produto, muito proximo de vencer") 
+}
+
+else if (porcentagem < 10 && produtos.Congelado == false){
+    console.log("Desconto!  Produto não congelado e próximo da validade")
+}
+
+else if (porcentagem < 5 && produtos.Congelado == true){
+    console.log("Desconto! Produto congelado e próximo da validade")
+}
+
+else {
+    console.log("Produto longe do vencimento.")
+}
